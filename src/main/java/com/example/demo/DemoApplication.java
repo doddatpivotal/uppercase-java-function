c6ab0817ca5ae0e531a7e7393ca0c4a0fb19af23
Dodd Pfeffer <dpfeffer@pivotal.io>
2018-08-10 15:15:16 -0400

    YOLO

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import reactor.core.publisher.Flux;
+
+import java.util.function.Function;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	@Bean
+	Function<Flux<String>, Flux<String>> uppercase() {
+		return incomingFlux -> incomingFlux.map(String::toUpperCase);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
